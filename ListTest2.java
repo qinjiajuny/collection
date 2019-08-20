8cf396dca16e7b9afda467b6dc06a026656bb69d
qinjiajun <505567352@qq.com>
2019-08-20 22:08:40 +0800

    按年龄排序但有编译错误

--- ListTest2.java
+++ ListTest2.java
@@ -1,38 +1,56 @@
 package collection;
-
-import java.util.LinkedList;
-
-class Person{
-	
-	String name;
-	
-	int age;
-
-	public Person(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
-	}
-	
-	@Override 
-	public String toString() {
-		return "{ 名字："+ this.name+" 年龄："+ this.age+"}";
-	}
-	
-}
-	
+/*报java.lang.NoClassDefFoundError错误
+ * 暂时不知道怎么解决
+ * */
+//import java.util.LinkedList;
+//
+//
+//class person{
+//	
+//	String name;
+//	
+//	int age;
+//
+//	public person(String name, int age) {
+//		super();
+//		this.name = name;
+//		this.age = age;
+//	}
+//	
+//	
+//	@Override 
+//	public String toString() {
+//		return "{ 名字："+ this.name+" 年龄："+ this.age+"}";
+//	}
+//	
+//}
+//
+//
 public class ListTest2 {
-
+//
 	public static void main(String[] args) {
-		LinkedList list = new LinkedList();
-		list.add(new Person("狗娃", 7));
-		list.add(new Person("狗剩", 17));
-		list.add(new Person("铁蛋", 3));
-		list.add(new Person("美美", 30));
-		
-		//编写一个函数根据人的年龄及逆行排序存储。
-		
-
+//		LinkedList list = new LinkedList();
+//		list.add(new person("狗娃", 7));
+//		list.add(new person("狗剩", 17));
+//		list.add(new person("铁蛋", 3));
+//		list.add(new person("美美", 30));
+//		
+//		//编写一个函数根据人的年龄及逆行排序存储。
+//		for(int i= 0 ; i<list.size() -1 ; i++){
+//			for(int j = i+1 ; j<list.size() ; j++){
+//				//符合条件交换位置
+//				person p1 = (person) list.get(i);
+//				person p2 = (person) list.get(j);
+//				if(p1.age>p2.age){
+//					//交换位置
+//					list.set(i, p2);
+//					list.set(j, p1);
+//					
+//				}
+//			}
+//		}
+//		System.out.println(list);	
 	}
-
+	
+	
 }
